8d99a96a015062889f4dbc69f9ce1eb218b98f5b
JackRaisch16 <102323161+JackRaisch16@users.noreply.github.com>
2024-01-17 10:47:17 -0500

    Update random.c

--- random.c
+++ random.c
@@ -1,4 +1,8 @@
-^X
 
-X
+
+char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int letterSize = sizeof(letters)/sizeof(letters[0]);
+int randomIndex = rand() % arraySize;
+return letters[randomIndex];
+}
 
